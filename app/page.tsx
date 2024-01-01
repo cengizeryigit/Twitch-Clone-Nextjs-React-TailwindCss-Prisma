927a5ea168dcd39d6698c94a72fa249c15ab58e7
Cengiz ERYİĞİT <cengiz_eryigit@hotmail.com>
2024-01-01 13:00:00 +0300

    2: folders

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
-export default function Home() {
+export default function Page() {
   return (
     <Button size="lg" >
-      Click ME
+      Click Me
     </Button>
   )
 }
