214be0a329c3fa99aa45bd49a539581317b8abe3
Cengiz ERYİĞİT <cengiz_eryigit@hotmail.com>
2024-01-02 14:00:00 +0300

    4: dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs"
 
 export default function Page() {
   return (
     <div className="flex flex-col gap-y-4">
-    <h1>Dashboard</h1>
-    <UserButton
-      afterSignOutUrl="/"
-    />
-  </div>
+      <h1>Dashboard</h1>
+      <UserButton
+        afterSignOutUrl="/"
+      />
+    </div>
   )
 }
