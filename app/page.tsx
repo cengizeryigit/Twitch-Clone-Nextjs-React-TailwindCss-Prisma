bea6253eca1f26b24aa25cdd9f3b295860e79bad
Cengiz ERYİĞİT <cengiz_eryigit@hotmail.com>
2024-01-02 13:00:00 +0300

    3: auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <Button size="lg" >
-      Click Me
-    </Button>
+    <div className="flex flex-col gap-y-4">
+    <h1>Dashboard</h1>
+    <UserButton
+      afterSignOutUrl="/"
+    />
+  </div>
   )
 }
